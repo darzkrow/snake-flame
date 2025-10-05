5ffc46b06d9093381e06f03399e3d21669b4be44
Darzkrow <gfrancojf@gmail.com>
2025-10-05 09:33:50 -0400

    chore: commit todos los cambios pendientes

--- lib/game/snake_game.dart
+++ lib/game/snake_game.dart
@@ -3,7 +3,7 @@ import 'package:flame/game.dart';
 import 'package:flame/input.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
 
 
 import 'score/score_manager.dart';
